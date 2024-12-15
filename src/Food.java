5c648fb7f8d9ccb2de0cf1659950e2abc895d4dc
Asher <ahnashley29@gmail.com>
2024-12-15 14:55:53 -0800

    Project WIP - Day 2 Updated Food.java

--- src/Food.java
+++ src/Food.java
@@ -1,12 +1,56 @@
+/**
+ * The Food class represents a food item with name and price.
+ */
 public class Food {
     private String name;
     private int price;
-    private int prepTime; // prepTime is in minutes
-    // TODO 1: Build constructor taking in a String and int to initialize instance variables
 
-    // TODO 2: Make getters and setters for name and price
+    /**
+     * Constructs a new Food object with given name and price.
+     * @param name the name of food
+     * @param price the price of food
+     */
+    public Food(String name, int price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+    /**
+     * Gets name of food.
+     * return name the name of food
+     */
+    public String getName(){
+        return name;
+    }
 
-    // TODO 3: make toString returning "name is $price and takes prepTime minutes to prepare."
+    /**
+     * Sets the name of food.
+     * @param name of the food
+     */
+    public void setName(String name){
+        this.name = name;
+    }
+    /**
+     * Sets the price of food.
+     * @return price the price of food
+     */
+    public int getPrice(){
+        return price;
+    }
 
-    // TODO 4: Create javadoc comments for the class, constructor, and methods
+    /**
+     * Sets the price of food.
+     * @param price of the food
+     */
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    /**
+     * Returns a string representation of the food.
+     * @return a string representation of the food in the format "name is $price."
+     */
+    public String toString(){
+        return name + "is $" + price + ".";
+    }
 }
